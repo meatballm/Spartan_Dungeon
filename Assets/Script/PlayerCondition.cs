31b60e59fa73695f4068484231a1816785dcff72
meatballm <wh9555115@naver.com>
2025-05-21 10:29:53 +0900

    add : 강의에서 진행된 부분까지

---
+++ Assets/Script/PlayerCondition.cs
@@ -0,0 +1,59 @@
+using System;
+using UnityEngine;
+public interface IDamagable
+{
+    void TakePhysicalDamage(int damageAmount);
+}
+public class PlayerCondition : MonoBehaviour, IDamagable
+{
+    public UICondition uiCondition;
+
+    Condition health { get { return uiCondition.health; } }
+    Condition hunger { get { return uiCondition.hunger; } }
+    Condition stamina { get { return uiCondition.stamina; } }
+
+    public float noHungerHealthDecay;
+    public event Action onTakeDamage;
+
+    private void Update()
+    {
+        if (hunger.curValue <= 0f)
+        {
+            health.Subtract(noHungerHealthDecay * Time.deltaTime);
+        }
+
+        if (health.curValue <= 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        health.Add(amount);
+    }
+
+    public void Eat(float amount)
+    {
+        hunger.Add(amount);
+    }
+
+    public void Die()
+    {
+        Debug.Log("죽어버리다니 한심하구나.");
+    }
+    public void TakePhysicalDamage(int damageAmount)
+    {
+        health.Subtract(damageAmount);
+        onTakeDamage?.Invoke();
+    }
+    public bool UseStamina(float amount)
+    {
+        if (stamina.curValue - amount < 0)
+        {
+            return false;
+        }
+        stamina.Subtract(amount);
+        return true;
+    }
+}
